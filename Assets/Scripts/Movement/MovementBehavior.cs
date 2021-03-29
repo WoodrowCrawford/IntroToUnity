e8363dca07b9c01ab5a973f2ab4adc52cea3023d
WoodrowCrawford <woodrowcrawford03@gmail.com>
2021-03-29 16:53:36 -0500

    Started on flappy bird hell

---
+++ Assets/Scripts/Movement/MovementBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehavior : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+    }
+}
